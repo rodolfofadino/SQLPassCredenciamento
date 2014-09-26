3342215e655fd295fd299f08f693d520ae54c40c
RodolfoF <rodolfo@minhavida.com.br>
2014-09-25 23:39:15 -0300

    First commit

---
+++ SQLPass/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace SQLPass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.Label();
+            this.txtEspera = new System.Windows.Forms.Label();
+            this.txtAlunos = new System.Windows.Forms.Label();
+            this.txtInscritos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtEmail.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(184, 194);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(530, 53);
+            this.txtEmail.TabIndex = 0;
+            this.txtEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Enabled = false;
+            this.btnImprimir.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.Location = new System.Drawing.Point(769, 194);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(195, 164);
+            this.btnImprimir.TabIndex = 1;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtNome.Enabled = false;
+            this.txtNome.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(184, 305);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(530, 53);
+            this.txtNome.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(179, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "E-mail:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(179, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nome:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 654);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "inscritos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 705);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "alunos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 677);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "espera";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 733);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.AutoSize = true;
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(65, 733);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(0, 13);
+            this.txtTotal.TabIndex = 13;
+            // 
+            // txtEspera
+            // 
+            this.txtEspera.AutoSize = true;
+            this.txtEspera.Location = new System.Drawing.Point(65, 677);
+            this.txtEspera.Name = "txtEspera";
+            this.txtEspera.Size = new System.Drawing.Size(0, 13);
+            this.txtEspera.TabIndex = 12;
+            // 
+            // txtAlunos
+            // 
+            this.txtAlunos.AutoSize = true;
+            this.txtAlunos.Location = new System.Drawing.Point(65, 705);
+            this.txtAlunos.Name = "txtAlunos";
+            this.txtAlunos.Size = new System.Drawing.Size(0, 13);
+            this.txtAlunos.TabIndex = 11;
+            // 
+            // txtInscritos
+            // 
+            this.txtInscritos.AutoSize = true;
+            this.txtInscritos.Location = new System.Drawing.Point(65, 654);
+            this.txtInscritos.Name = "txtInscritos";
+            this.txtInscritos.Size = new System.Drawing.Size(0, 13);
+            this.txtInscritos.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtEspera);
+            this.Controls.Add(this.txtAlunos);
+            this.Controls.Add(this.txtInscritos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.txtEmail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label txtTotal;
+        private System.Windows.Forms.Label txtEspera;
+        private System.Windows.Forms.Label txtAlunos;
+        private System.Windows.Forms.Label txtInscritos;
+    }
+}
+
---
+++ SQLPass/cupom.cs
@@ -0,0 +1,92 @@
+﻿namespace SQLPass
+{
+    class Cupom
+    {
+
+        public void fichaDeAvaliacao()
+        {
+            Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<bmp></bmp>", 0);
+            Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<confgui>P</confgui>", 0);
+            for (int i = 0; i < 8; i++)
+            {
+
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<fe><b>Código da sessão:</b>_______________________" +
+                                                                   "<sl>1</sl><b>Seu nome:</b>_______________________________" +
+                                                                   "<sl>2</sl><b>Expectativa:</b>" +
+                                                                   "<sl>1</sl>    Não atendeu | Atendeu | Excedeu" +
+                                                                   "<sl>1</sl><b>Nota:</b>   1    2     3     4     5" +
+                                                                   "<ce><sl>2</sl><b>Impresso na DR700 DARUMA" +
+                                                                   "<l></l> www.desenvolvedoresdaruma.com.br</b></ce></fe>", 0);
+                if (i == 7)
+                {
+                    Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<confgui>T</confgui>", 0);
+                    Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<sl>3</sl><gui></gui>", 0);
+                }
+                else
+                    Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<sl>3</sl><gui></gui>", 0);
+            }
+        }
+
+        public void Imprimir(string Nome, string email)
+        {
+            {
+
+                //MessageBox.Show("Impressora Ligada!");
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<bmp></bmp>", 0);
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<ce><e><b><da>" + Nome + "<l></l></da></b>" +
+                                                                "<fe>" + email +
+                                                                "<sl>1</sl></e></fe> Este é o seu comprovante!!!" +
+                                                                "<l></l>Impresso na DR700 DARUMA" +
+                                                                "<l></l> www.desenvolvedoresdaruma.com.br</ce>", 0);
+
+
+
+                //PULANDO LINHA PARA LOGO
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<sl>2</sl>", 0);
+
+                //SIOS
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<gui></gui>", 0);
+
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<ce><e><b><da> SIOS <sl>2</sl></da>" +
+                                                                Nome + "<l></l></b>" +
+                                                                "<fe>" + email + "</fe></e>" +
+                                                                "<sl>1</sl> Deposite este ticket na urna SIOS!" +
+                                                                "<sl>1</sl><b>Impresso na DR700 DARUMA" +
+                                                                "<l></l> www.desenvolvedoresdaruma.com.br</ce>", 0);
+
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<sl>3</sl><gui></gui>", 0);
+
+                //NGR Solutions
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<ce><e><b><da> NGR Solutions </b></da>" +
+                                                                "<sl>2</sl><b>" + Nome + "<l></l>" +
+                                                                "<fe>" + email + "</b></e></fe>" +
+                                                                "<sl>1</sl> Deposite este ticket na urna NGR Solutions!" +
+                                                                "<sl>1</sl><b>Impresso na DR700 DARUMA" +
+                                                                "<l></l> www.desenvolvedoresdaruma.com.br</ce>", 0);
+
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<sl>3</sl><gui></gui>", 0);
+
+                //Idera
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<ce><e><b><da> Idera </b></da>" +
+                                                                "<sl>2</sl><b>" + Nome + "<l></l>" +
+                                                                "<fe>" + email + "</b></e></fe>" +
+                                                                "<sl>1</sl> Deposite este ticket na urna Idera!" +
+                                                                "<sl>1</sl><b>Impresso na DR700 DARUMA" +
+                                                                "<l></l> www.desenvolvedoresdaruma.com.br</ce>", 0);
+
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<sl>3</sl><gui></gui>", 0);
+
+                //Mainworks Confio
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<ce><e><b><da> Mainworks Confio </b></da>" +
+                                                                "<sl>2</sl><b>" + Nome + "<l></l>" +
+                                                                "<fe>" + email + "</b></e></fe>" +
+                                                                "<sl>1</sl> Deposite este ticket na urna Mainworks Confio!" +
+                                                                "<sl>1</sl><b>Impresso na DR700 DARUMA" +
+                                                                "<l></l> www.desenvolvedoresdaruma.com.br</ce>", 0);
+
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<sl>3</sl><confgui>T</confgui><gui></gui>", 0);
+                Declaracoes.iImprimirTexto_DUAL_DarumaFramework("<confgui>P</confgui>", 0);
+            }
+        }
+    }
+}
